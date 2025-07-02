282a0db252bf1f62e2917c0a0a7bb8064664c9ba
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-02 13:36:49 +0300

    exercise 2 Fezz - varianta 1

--- intex.ts
+++ intex.ts
@@ -1,11 +1,28 @@
 // This is our main function
+function fezz(message: string):string{
+    let ok:boolean = false;
+    for(let i = 0 ; i < message.length ; i++){
+        if(message[i] == 'B'){
+            message = [message.slice(0, i), 'Fezz', message.slice(i)].join('');
+            ok = true;
+            break;
+        }
+    }
+    if(ok == false){
+        message = message.concat('Fezz');
+    }
+    return message;
+}
 
 function fizzbuzz(): void {
     let message: string;
-    for(let i = 1; i <= 100; i++ ){
+    for(let i = 1; i <= 455; i++ ){
         message = i.toString();
          if(i % 11 === 0){
              message = "Bong";
+             if(i % 13 === 0){
+                 message = fezz(message);
+             }
          }
          else if(i % 3 === 0){
              message = "Fizz";
@@ -15,16 +32,26 @@ function fizzbuzz(): void {
              if(i % 7 === 0){
                  message = message.concat("Bang");
              }
+             if(i % 13 === 0){
+                 message = fezz(message);
+             }
         }
         else if(i % 5 === 0){
              message = "Buzz"
              if(i % 7 === 0){
                  message = message.concat("Bang");
+                 if(i % 13 === 0)
+                     message = fezz(message);
              }
         }
         else if(i % 7 === 0){
             message = "Bang";
+                 if(i % 13 === 0)
+                     message = fezz(message);
         }
+        else if(i % 13 === 0){
+            message = "Fezz";
+         }
         console.log(message);
     }
 }
