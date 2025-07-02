5d7ecefb38d2787956ae2426c1380c84f24bd23c
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-02 10:43:52 +0300

    A useful message explaining the work that has been done in this batch

--- intex.ts
+++ intex.ts
@@ -1,7 +1,19 @@
 // This is our main function
 function fizzbuzz(): void {
-    console.log("Hello, World!");
-    // Put your code here...
+    for(let i = 1; i <= 100; i++ ){
+        if(i % 3 == 0){
+            if(i % 5 == 0){
+                console.log("FizzBuzz");
+            }
+            else
+                console.log("Fizz");
+        }
+        else if(i % 5 == 0){
+            console.log("Buzz");
+        }
+        else
+            console.log(i);
+    }
 }
 
 // Now, we run the main function:
